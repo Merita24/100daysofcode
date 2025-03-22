d97600e95d971ea6e774ecfeb21cb6da53846533
Merita24 <meritaodera@gmail.com>
2025-03-22 14:00:20 +0300

    inital commit

--- BS.py
+++ BS.py
@@ -2,8 +2,9 @@ def binary_search(arr,target):
     
     low=0
     high=len(arr)-1
-    mid=low-high//2
     while low<=high:
+        mid=low-high//2
+    
         if arr[mid]==target:
             return mid
         elif arr[mid]<target:
