120df2c6d96b5e404a779410c63eee3a9b70e3c8
Merita24 <meritaodera@gmail.com>
2025-03-22 13:52:53 +0300

    initial commit

---
+++ BS.py
@@ -0,0 +1,18 @@
+def binary_search(arr,target):
+    
+    low=0
+    high=len(arr)-1
+    mid=low-high//2
+    while low<=high:
+        if arr[mid]==target:
+            return mid
+        elif arr[mid]<target:
+             low=mid+1
+        else:
+            high=mid-1
+    return -1
+
+sorted_list=[1,2,4,5,6,7,8,9,10,11,12,14,16,17,18,19,22,26]
+target=5
+result=binary_search(sorted_list,target)
+print(result)
